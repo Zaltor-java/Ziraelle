ffc60b4492c5eae325aa9f7d802ff976f9902881
Alexander <76738219+Zaltor-java@users.noreply.github.com>
2021-03-06 19:48:37 +0300

    comments edit

--- Ziraelle/Scram.java
+++ Ziraelle/Scram.java
@@ -2,10 +2,16 @@ package Ziraelle;
 public class Scram {
     String scram="";
     //ключ шифрования
+    // encrypt key
     final long key = 9982917478858578L;
     public String scramming(String text) {
-        // Цикл, обрабатывающий каждый символ ввода и прогоняющий его через ключ путём исключающего НЕ,
-        // при первом прогоне сообщение шифруется, а при втором расшифровывается (Симметричное шифрование)
+        /*
+        Цикл, обрабатывающий каждый символ ввода и прогоняющий его через ключ путём исключающего НЕ,
+        при первом прогоне сообщение шифруется, а при втором расшифровывается (Симметричное шифрование)
+        
+        A loop that processes each input character and runs it through the key with an exclusive NOT,
+         on the first run, the message is encrypted, and on the second, it is decrypted (Symmetric encryption)
+        */
         for (int i = 0; i < text.length(); i++) {
             scram += (char) (text.charAt(i) ^ key);
         }
