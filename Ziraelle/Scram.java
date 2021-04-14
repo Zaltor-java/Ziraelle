9deb72b46e577d066115cf9b8ccae68364c074f4
Zaltor-java <76738219+Zaltor-java@users.noreply.github.com>
2021-04-14 17:04:07 +0300

    Add test

--- Ziraelle/Scram.java
+++ Ziraelle/Scram.java
@@ -1,25 +1,13 @@
 package Ziraelle;
 
 public class Scram {
-    String scram = "";
-    private final String text;
-    private final long key;
+    String scram="";
 
-    public Scram(String text, long key) {
-        this.text = text;
-        this.key = key;
-    }
-
-    public String scramming() {
-        /*
-        Цикл, обрабатывающий каждый символ ввода и прогоняющий его через ключ путём исключающего ИЛИ (XOR),
-        при первом прогоне сообщение шифруется, а при втором расшифровывается (Симметричное шифрование)
-        
-        A loop that processes each input character and runs it through the key with an XOR,
-         on the first run, the message is encrypted, and on the second, it is decrypted (Symmetric encryption)
-        */
+    public String scramming(String text, long key) {
+      /* A loop that processes each input character and runs it through the key with an XOR,
+         on the first run, the message is encrypted, and on the second, it is decrypted (Symmetric encryption) */
         for (int i = 0; i < text.length(); i++) {
-            scram += (char) (text.charAt(i) ^ key);
+            scram += (char)(text.charAt(i) ^ key);
         }
         return scram;
     }
--- Ziraelle/Scrambler.java
+++ Ziraelle/Scrambler.java
@@ -1,45 +1,45 @@
 package Ziraelle;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Scrambler {
-    public static void main(String[] args)  {
-        Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         long key;
         String endInput;
-        System.out.println("Добро пожаловать в программу \"Ziraelle\", Что вы хотите сделать?"); // Ziraelle - ласточка. swallow bird
+        System.out.println("Welcome to the  \"Ziraelle\", What are you want?"); // Ziraelle - swallow bird
         do {
-            System.out.println("1.Зашифровать ");
-            System.out.println("2.Расшифровать");
-            // пользовательский ввод на дальнейшее поведение программы
-            int inputChoise = scn.nextInt();
+            System.out.println("1.Encrypt ");
+            System.out.println("2.Decrypt");
+            // user input for further program behavior
+            int inputChoise = Integer.parseInt(reader.readLine());
             switch (inputChoise) {
                 case (1):
-                    System.out.print("Введите текст для шифрования: ");
-                    String scramText = scn.next();
+                    System.out.print("Enter text for encrypt: ");
+                    String scramText = reader.readLine();
 
-                    System.out.print("Придумайте ключ шифрования (Числовое): ");
-                    key = scn.nextLong();
-
-                    // передача ввода в класс scram и его дальнейшее шифрование
-                    Scram scram = new Scram(scramText, key);
-                    System.out.println(scram.scramming());
+                    System.out.print("Enter crypt key (Number): ");
+                        key = Long.parseLong(reader.readLine());
+                        // passing input to the scram class and encrypting it further
+                        Scram scram = new Scram();
+                        System.out.println(scram.scramming(scramText, key));
                     break;
                 case (2):
-                    System.out.print("Введите текст для расшифровки: ");
-                    String descramText = scn.next();
+                    System.out.print("Enter text for deencrypt: ");
+                    String descramText = reader.readLine();
 
-                    System.out.print("Введите ключ шифрования: ");
-                    key = scn.nextLong();
-
-                    Scram descram = new Scram(descramText, key);
-                    System.out.println(descram.scramming());
+                    System.out.print("Enter crypt key (Number): ");
+                    key = Long.parseLong(reader.readLine());
+                    Scram descram = new Scram();
+                    System.out.println(descram.scramming(descramText, key));
                     break;
                 default:
-                    System.out.println("Такого варианта нет");
+                    System.out.println("There is no such option");
             }
-            System.out.println("Хотите повторно воспользоваться программой? (y/n)");
-            endInput = scn.next().toLowerCase();
+            System.out.println("Want to reuse the program? (y/n)");
+            endInput = reader.readLine().toLowerCase();
         } while (endInput.equals("y"));
     }
 }
